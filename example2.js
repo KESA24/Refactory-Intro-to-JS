9bf21f2092b473fbf802710960cb2185854dd721
Kesa <nekepatri@gmail.com>
2020-09-23 10:44:28 +0300

    still checking datata type, my Nan aint working

--- example2.js
+++ example2.js
@@ -6,6 +6,17 @@ const num = 10;
 const num2 = 20;
 const sum = (num+num2);
 
+const num3 = "d";
+const num4 = [5+3];
+let a;
+let b;
+let c = a + b;
 //num = 100;
 console.log(sum);
-console.log (typeof(num));
+//Checking the datatype of num
+//datatypes: number,string,objects,undefined, Nan(not a number)
+console.log (typeof(num)); //number
+console.log (typeof(num3)); //string, has quotes
+console.log (typeof(num4)); // object, placed in brackets
+console.log(typeof(num5));//undefined, c is not defined
+console.log (typeof(c));
