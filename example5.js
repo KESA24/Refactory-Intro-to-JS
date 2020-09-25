0253ab1811d2c5a012d3f956a9f052919c1de3cf
Kesa <nekepatri@gmail.com>
2020-09-25 09:51:26 +0300

    learnt another way of defining a function

--- example5.js
+++ example5.js
@@ -5,7 +5,7 @@
 //Function definition
 function addNum(){
     const num1 = 2, num2=4;
-    let sum = num1+num1;
+    let sum = num1+num2;
 
     console.log(sum);
 }
@@ -15,4 +15,13 @@ function addNum(){
 //{function definition}
 
 // calling a function / function invoketion
-addNum()
+//addNum();
+
+//Another way of function definition
+function addNum2( num1, num2){
+   const addition = num1+num2;
+   console.log (addition);
+}
+
+addNum2(5,8);
+addNum2(20,27)
