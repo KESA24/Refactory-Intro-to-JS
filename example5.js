41907641e4b6b6ff6ef7e598dc58bd8f7192a298
Kesa <nekepatri@gmail.com>
2020-09-25 09:38:45 +0300

    Started on conditions and loops under functions in Javascript

---
+++ example5.js
@@ -0,0 +1,18 @@
+//Conditions and Loops under functions
+
+//a function is a group of statements  that peform a given task and have a given name
+
+//Function definition
+function addNum(){
+    const num1 = 2, num2=4;
+    let sum = num1+num1;
+
+    console.log(sum);
+}
+
+//{} -brace; ()- parenthesis
+//A function will not run until we call it
+//{function definition}
+
+// calling a function / function invoketion
+addNum()
