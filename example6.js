17e0e128473efa9fe29248dc742d5ca93bbe1c4f
Kesa <nekepatri@gmail.com>
2020-09-25 10:50:20 +0300

    my Nan started wroking!!!, I updated vs code btw

--- example6.js
+++ example6.js
@@ -5,3 +5,14 @@ function checkNum (num1){
         console.log("Number is " + num1);
     }
 }
+
+checkNum(10);
+checkNum(7);
+
+//Conditions are the best way to let the computer imagine by using control statements
+//Parameterised function is a function that takes on a parameter
+
+let a;
+let b;
+let c = a+b;
+console.log (c);
