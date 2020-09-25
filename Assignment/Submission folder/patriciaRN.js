c0f7406cddb132a7562ae6f33b5199e3b083f3ef
Kesa <nekepatri@gmail.com>
2020-09-25 17:41:39 +0300

    Patricia Rhoda Nekesa's submission to the Assignment

--- Assignment/Submission folder/patriciaRN.js
+++ Assignment/Submission folder/patriciaRN.js
@@ -6,10 +6,10 @@ var name;
 The reserved space above now has a value of type string as " ozzy zainab".*/
 name = " ozzy zainab";
 
-//Outputs the value in the variable-name in console.
+//Outputs the value in the variable-name in the terminal.
 console.log(name);
 
-// Outputs the string "<br/>"  in the console.
+// Outputs the string "<br/>"  in the terminal.
 console.log("<br/>");
 
 /*Declares another variable "name", no conflict with earlier variable due to keyword "var".
@@ -19,7 +19,7 @@ var name;
 // Initialises the  new  variable with string value - "ozzy emma otim".
 name = "ozzy emma otim ";
 
-//Outputs the value assigned in the console, in  this instance  the output will be, "ozzy emma otim".
+//Outputs the value assigned in the terminal, in  this instance  the output will be, "ozzy emma otim".
 console.log(name);
 
 /*Declares another variable "name", no conflict with earlier variables due to keyword "var".
@@ -29,7 +29,7 @@ var name;
 // Initialises the  new  variable  name with string value - "marrie jacob ozyy"
 name = "marrie jacob ozyy";
 
-//Outputs the value assigned in the console, in  this instance  the output will be, "marrie jacob ozyy"
+//Outputs the value assigned in the terminal, in  this instance  the output will be, "marrie jacob ozyy"
 console.log(name);
 
 /*Declares another variable "name", no conflict with earlier variables due to keyword "var".
@@ -39,7 +39,7 @@ var name;
 // Initialises the  new  variable with string value - " ozyy diane".
 name = " ozzy diane";
 
-//Outputs the value assigned in the console, in  this instance  the output will be, " ozyy diane" .
+//Outputs the value assigned in the terminal, in  this instance  the output will be, " ozyy diane" .
 console.log(name);
 
 /*Declares another variable "name", no conflict with earlier variables due to keyword "var".
@@ -49,7 +49,7 @@ var name
 //Initialises the  new  variable with string value "joanita paul ozzy"
 name = "joanita paul ozzy ";
 
-//Outputs the value assigned in the console, in  this instance  the output will be, "joanita paul ozzy"
+//Outputs the value assigned in the terminal, in  this instance  the output will be, "joanita paul ozzy"
 console.log(name);
 
 //Declares a new variable "age"; computer reserves memory for this variable called "age"
@@ -84,5 +84,5 @@ document.write(x);
 // declares another variable x and intialises it with an undefined value
 var x = undefined;
 
-//Outputs the value of x in the console, in this instance it will be a string "undefined"
+//Outputs the value of x in the terminal, in this instance it will be a string "undefined"
 console.log(x);
