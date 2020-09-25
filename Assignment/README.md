1d0d5420d86e559e5dca7a911aac13135af95996
Kesa <nekepatri@gmail.com>
2020-09-25 17:18:45 +0300

    First Assignment, Completed the code explanation one, to start on PAYE soon

---
+++ Assignment/README.md
@@ -0,0 +1,2 @@
+# refactoryCohort6
+ First Javascript Assignment to describe what each line of code does as written in the firstAssgn.js file
---
+++ PAYE.js
@@ -0,0 +1,4 @@
+//Write a function to calculate employees's PAYE in Uganda.
+//NSSF = 11% of pay, Income tax = 30% of balance after nssf.
+//Do this and post assignment to the repo
+//PAYE AND NSSF calculator
