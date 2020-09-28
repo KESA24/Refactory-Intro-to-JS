17e77ed0e7b82990ddac597c31df8d95271808a0
Kesa <nekepatri@gmail.com>
2020-09-28 09:50:07 +0300

    Started on arrays

---
+++ example7.js
@@ -0,0 +1,12 @@
+
+
+function myNum (a,b,c){
+    c = Array (a,b,c);
+    console.log(c[0])
+}
+
+myNum(11,12,5);
+
+//An array is a pile of values in one space
+//A variable having one or more values
+//The size of an array is (n-1), n being the total number of values, the first value is indexed starting from 0;
