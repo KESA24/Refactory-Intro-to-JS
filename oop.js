0ae02953ab11a8733c2f44e7c4047f96f99c4a9f
Kesa <nekepatri@gmail.com>
2020-09-29 11:50:05 +0300

    Started on object oriented programming

---
+++ oop.js
@@ -0,0 +1,19 @@
+//Object Oriented Programming: This is a concept of creation of software.  
+//OOP consists of objects and classes.
+//An object is an instance of a class. 
+//A class is a blueprint of an object.
+//Basing on real world objects, most tech companies are named by things we all know eg apple,windows,blackberry.
+
+
+var myoop = {
+    name: "SUBARU Legacy B4",
+    make: "JAPAN",
+    engine: "EJ25 Turbo",
+    yearOfMake: 2011,
+}
+ 
+// We use a dot(.) to access a property/attribute/characteristic of an object
+console.log(myoop.make);
+myoop.year 
+
+
