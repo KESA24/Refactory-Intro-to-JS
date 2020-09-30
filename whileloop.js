8d2418c112ea03e84522c12faf61f0ed8702e8f9
Kesa <nekepatri@gmail.com>
2020-09-30 11:00:51 +0300

    file location changed

--- whileloop.js
+++ whileloop.js
@@ -1,3 +1,4 @@
+
 //The while loop loops through a block of codee as long a specified condition is true
 
 let i=0;
@@ -6,5 +7,12 @@ let text = "";
 while (i <= 10){
     text = "The number is" + i;
     i++;
+    console.log(text);
+}
+
+//The For loop.
+
+for(i=0; i <=10; i++){
+    text = "The number is" + i;
     console.log(text);
 }
