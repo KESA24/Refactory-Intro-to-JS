69a570e4a1adb878be73ee0aa7b76ae425b8eb8a
Kesa <nekepatri@gmail.com>
2020-09-30 09:33:24 +0300

    Introduced to the use of while loop

---
+++ whileloop.js
@@ -0,0 +1,10 @@
+//The while loop loops through a block of codee as long a specified condition is true
+
+let i=0;
+let text = "";
+
+while (i <= 10){
+    text = "The number is" + i;
+    i++;
+    console.log(text);
+}
