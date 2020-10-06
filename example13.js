64e4522487bc7e52f4ff52a32f475a192cbc9c7a
Kesa <nekepatri@gmail.com>
2020-10-06 09:58:24 +0300

    Accessing elements in Javascript using the DOM

---
+++ example13.js
@@ -0,0 +1,5 @@
+
+//Javascrpt code is loaded better when it's in the body or below the body.
+//JS in the head tag takes long or does not load in the pages.
+const demo = document.getElementById("demo"); 
+demo.style.border = "3px solid purple"
