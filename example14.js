84429fa2af46c09daf0cfd906ecc7c18c4020922
Kesa <nekepatri@gmail.com>
2020-10-07 10:24:36 +0300

    Created a div using a JS function

--- example14.js
+++ example14.js
@@ -9,3 +9,24 @@ block = document.createElement("div");
 block.innerHTML = "This div was created/inserted using javascript";
 container = document.getElementById("container");
 container.appendChild(block);
+
+block.style.color = "red";
+block.style.border = "1px solid yellow"
+block.style.height = "40px"
+
+//Make the above code a function
+
+function creatediv(){
+    var block;
+    var container;
+    block = document.createElement("div");
+    block.innerHTML = "This div was created/inserted using  a javascript function";
+    container = document.getElementById("container");
+    container.appendChild(block);
+    
+    block.style.color = "red";
+    block.style.border = "1px solid yellow"
+    block.style.height = "40px"
+}
+
+creatediv();
