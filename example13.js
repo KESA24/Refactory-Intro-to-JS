0b30069723b3c97259a4820f71d8354145d4d91f
Kesa <nekepatri@gmail.com>
2020-10-07 09:48:29 +0300

    Aceessing elements in HTML using JavaScript

--- example13.js
+++ example13.js
@@ -1,8 +1,7 @@
-
 //Javascrpt code is loaded better when it's in the body or below the body.
 //JS in the head tag takes long or does not load in the pages.
-const demo = document.getElementById("demo"); 
-demo.style.border = "3px solid purple"
+const demo = document.getElementById("demo");
+demo.style.border = "3px solid purple";
 
 const classDemo = document.getElementsByClassName("demo");
 //We have two elements with the same class name, so we have an array and below we access them individually but we can also loop through.
@@ -10,13 +9,13 @@ const classDemo = document.getElementsByClassName("demo");
 //classDemo[1].style.border = "3px solid orange";
 
 //The loop
-for (item = 0; item < classDemo.length; item++){
-    classDemo[item].style.border = "3px solid blue";
+for (item = 0; item < classDemo.length; item++) {
+  classDemo[item].style.border = "3px solid blue";
 }
 
 const demoTag = document.getElementsByTagName("article");
-for (item = 0; item < demoTag.length; item++){
-    demoTag[item].style.border = "2px solid red";
+for (item = 0; item < demoTag.length; item++) {
+  demoTag[item].style.border = "2px solid red";
 }
 
 //Do not forget the dot(.) and hashtag(#) to identif the classes and id
@@ -25,6 +24,6 @@ demoQuery.style.border = "2px solid yellow";
 
 //Using For each to access elements.  Do not forget the (.)dot to access a class.
 const demoQueryAll = document.querySelectorAll(".demo-query-all");
-demoQueryAll.forEach(item => {
-    item.style.border = "2px solid green";
-})
+demoQueryAll.forEach((item) => {
+  item.style.border = "2px solid green";
+});
