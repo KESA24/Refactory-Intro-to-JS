58ef4bbd7a382a4a62728499d136dde460d257e6
Kesa <nekepatri@gmail.com>
2020-10-08 14:08:51 +0300

    Studied the chalk library

---
+++ chalk/example.js
@@ -0,0 +1,10 @@
+//Import chalk package
+const chalk = require('chalk')
+
+//Declare variable chalking
+var chalking;
+
+//Style a string
+console.log(chalk.blue('Hello world'))
+chalking1 = chalk.rgb(255,255,0)('Hello') + "W"
+
